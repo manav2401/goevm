003a15525f9fdda72562972d6a4bbb94289a3406
Manav Darji <manavdarji.india@gmail.com>
2024-07-11 20:45:30 +0530

    tests: add simple memory and stack tests (#3)

---
+++ tests/memory_test.go
@@ -0,0 +1,23 @@
+package tests
+
+import (
+	"goevm/evm"
+	"testing"
+)
+
+func TestMemory(t *testing.T) {
+	memory := evm.NewMemory()
+	memory.InitWithSize(10)
+	data := "hello"
+	memory.Store(0, 5, []byte(data))
+	data = "world"
+	memory.Store(5, 5, []byte(data))
+	len := memory.Len()
+	if len != 10 {
+		t.Fatalf("Invalid memory length, expected: %d, got: %d", 5, len)
+	}
+	storedData := memory.Load(0, 10)
+	if string(storedData) != "helloworld" {
+		t.Fatalf("Invalid stored data in memory, expected: %s, got: %s", "helloworld", string(storedData))
+	}
+}
---
+++ tests/stack_test.go
@@ -0,0 +1,32 @@
+package tests
+
+import (
+	"goevm/evm"
+	"testing"
+
+	"github.com/holiman/uint256"
+)
+
+func TestStack(t *testing.T) {
+	stack := evm.NewStack()
+	one := uint256.NewInt(1)
+	two := uint256.NewInt(1)
+	stack.Push(one)
+	stack.Push(two)
+	top := stack.Peek()
+	if top.Cmp(two) != 0 {
+		t.Fatalf("Invalid peer, expected: %v and got: %v", two.Uint64(), top.Uint64())
+	}
+	top = stack.Pop()
+	if top.Cmp(two) != 0 {
+		t.Fatalf("Invalid pop, expected: %v and got: %v", two.Uint64(), top.Uint64())
+	}
+	top = stack.Peek()
+	if top.Cmp(one) != 0 {
+		t.Fatalf("Invalid peek, expected: %v and got: %v", one.Uint64(), top.Uint64())
+	}
+	top = stack.Pop()
+	if top.Cmp(one) != 0 {
+		t.Fatalf("Invalid pop, expected: %v and got: %v", one.Uint64(), top.Uint64())
+	}
+}
